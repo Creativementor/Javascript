fca209021dc86fae8626211cff81ae46b9b5182b
Creativementor <muhammadhamza318@gmail.com>
2024-02-25 12:56:59 +0500

    Code Push
    Code Push

---
+++ script.js
@@ -0,0 +1,31 @@
+// Condition addtion,substration,multiplication and division
+
+// var number1 = 50
+// var number2 = 50
+// var addmessage = "Here is your number"
+// var subtratemessage = "Here is your subtrate"
+// var mulptilicationmessage = "Here is your mulptilication"
+// var divisionmessage = "Here is your division"
+
+// alert(addmessage + (50 + 50))
+// alert(subtratemessage + (50 - 50))
+// alert(mulptilicationmessage + (50 * 50))
+// alert(divisionmessage + (50 / 50))
+
+
+
+// Percentage condition
+// pecentage formula
+
+// alert((obtainmarks / totalmarks * 100) + "%")
+
+// Solution
+
+// var percentage = ((45/50)*100) + "%"
+
+// alert(percentage)
+
+var totalmarks = 50
+var obtainmarks = 45
+
+alert((obtainmarks /totalmarks) *100 + "%")
