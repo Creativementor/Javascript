a7976381a682885ebcbc6fb2b1de88dc1ee7850c
Creativementor <muhammadhamza318@gmail.com>
2024-03-12 17:56:24 +0500

    commit
    commit

--- script.js
+++ script.js
@@ -525,19 +525,30 @@
 
 
 var percentage = +prompt("enter the percentage")
-
-if (percentage >= 40 && percentage <= 49) {
-    console.log("D Grade");
-} else if (percentage >= 50 && percentage <= 59) {
-    console.log("C Grade");
-} else if (percentage >= 60 && percentage <= 69) {
-    console.log("B Grade");
-} else if (percentage >= 70 && percentage <= 79) {
-    console.log("A Grade");
-} else if (percentage <= 100) {
+if (percentage >= 100) {
+    console.log("this not a percentage");
+} else if (percentage <= 100 && percentage >= 80) {
     console.log("A+");
+} else if (percentage >= 70 && percentage < 80) {
+    console.log("A Grade");
+} else if (percentage >= 60 && percentage < 70) {
+    console.log("B Grade");
+} else if (percentage >= 50 && percentage < 60) {
+    console.log("C Grade");
+} else if (percentage >= 40 && percentage < 50) {
+    console.log("D Grade");
 } else if (percentage <= 40) {
-    console.log("Not a percentage");
-} else if (percentage > 100) {
-    console.log("This not a percentage");
+    console.log("your are fail");
 }
+
+
+// var num = prompt("Add number")
+// var num1 = prompt("Add table number")
+
+// var reminder = num % 4
+
+// if (reminder === 0) {
+//     console.log(num + " number is divided by " + num1);
+// } else {
+//     console.log(num + " number isn't divided by " + num1)
+// }
